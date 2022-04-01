e8a74c2eb4d1d59fb4d8e4056fe24739847d67a3
SimonM <menasim1@students.zhaw.ch>
2022-04-01 15:10:36 +0200

    Graphs

---
+++ Orbitarium-Display-API/wwwroot/js/graph.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+class Graph {
+    constructor(height, width, color, rangeMax, rangeMin) {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+        this.max = rangeMax;
+        this.min = rangeMin;
+    }
+
+    get style() {
+        return `height: ${this.height}px; width: ${this.width}px`;
+    }
+
+    render() {
+        return `<div class='graph' style="${this.style}">
+            <span class="graph-fill" style="background-color: ${this.color}; ${this.style};"></span>
+        </div>`;
+    }
+}
---
+++ Orbitarium-Display-API/wwwroot/js/index.js
@@ -0,0 +1,16 @@
+﻿const HEIGHT = 500;
+const WIDTH = 300;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const co2 = new Graph(HEIGHT, WIDTH, "red", );
+    const temperature = new Graph(HEIGHT, WIDTH, "green");
+    const sealevel = new Graph(HEIGHT, WIDTH, "blue");
+    const popUnderWater = new Graph(HEIGHT, WIDTH, "yellow");
+    
+    document.getElementById("co2").insertAdjacentHTML("beforeend", co2.render());
+    document.getElementById("temperature").insertAdjacentHTML("beforeend", temperature.render());
+    document.getElementById("sealevel").insertAdjacentHTML("beforeend", sealevel.render());
+    document.getElementById("population-under-water").insertAdjacentHTML("beforeend", popUnderWater.render());
+
+});
