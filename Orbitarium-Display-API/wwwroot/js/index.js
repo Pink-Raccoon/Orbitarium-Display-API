4303087f9c2255d7e767d435d347324c85098aec
Simon Mena <menasim1@students.zhaw.ch>
2022-04-21 12:26:28 +0200

    static population data

--- Orbitarium-Display-API/wwwroot/js/index.js
+++ Orbitarium-Display-API/wwwroot/js/index.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     co2 = new Graph(HEIGHT, WIDTH, "#ceffcf", 0, 1000, "CO2", 'ppm', [0,200, 400, 600, 800, 1000], 412.5);
     temperature = new Graph(HEIGHT, WIDTH, "#fffebb", 0, 40, "Global Mean Temperature", '°C', [0, 10, 20, 30, 40], 13.9);
     seaLevel = new Graph(HEIGHT, WIDTH, "#b6cdff", -120, 80, "Sea Level", 'm', [-120, -60, 0, 80], 0.091);
-    popUnderWater = new Graph(HEIGHT, WIDTH, "#ffb6fe", 0, 20, "Population Under Water", 'mil', [0,5,10,15,20], 10);
+    popUnderWater = new Graph(HEIGHT, WIDTH, "#ffb6fe", 0, 10000, "Population Under Water", 'mil', [0,2500, 5000,10000], 7953);
     
     document.getElementById("co2").insertAdjacentHTML("beforeend", co2.render());
     document.getElementById("temperature").insertAdjacentHTML("beforeend", temperature.render());
@@ -41,8 +41,7 @@ function fetchData() {
         .then((response) => {
             return response.json();
         })
-        .then((displayData) => {            
-            // console.log(displayData)
+        .then((displayData) => {
 
             yearDOMElement.innerHTML = `Year: ${displayData.year}`;            
             co2.graphValue = displayData.co2ppm;
@@ -51,13 +50,16 @@ function fetchData() {
             seaLevel.graphValue = displayData.seaLevel;
             popUnderWater.graphValue = displayData.populationUnderWater;
             
+            /*
+            // TODO: dynamic population data from backend
             popUnderWater.max = displayData.population;
+            
             const popLabels = []
             for( let i = 0, firstLabel = 0; i <= 4; i++, firstLabel += Math.round(displayData.population/4  * 100) / 100) {
                 popLabels.push(firstLabel);
             }
             popUnderWater.labels = popLabels;
-
+            */
             dataValid = true;
         })
         .catch(error => {
